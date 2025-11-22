c6f69253cc14c4bba8e6319c58f43d06277a28bc
mlouzet <micklouzet@hotmail.fr>
2025-11-22 21:58:37 +0100

    remove useless file

--- zenora.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Zenora\Zenora;
-use Zenora\Theme\CyberTheme;
-
-return Zenora::forge()
-  // ->withTheme(new CyberTheme())
-  // Adjust the path and namespace for your commands:
-  ->discover(__DIR__ . '/src/Command', 'App\\Command');
